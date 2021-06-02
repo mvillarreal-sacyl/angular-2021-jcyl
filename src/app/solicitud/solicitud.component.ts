eb21d7fe311d0f418540201ef5a87c9bf37687a7
manuelvgv <manuelvgv@gmail.com>
2021-06-02 13:47:07 +0200

    Primer commit ejercicio 3

---
+++ src/app/solicitud/solicitud.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitud',
+  templateUrl: './solicitud.component.html',
+  styleUrls: ['./solicitud.component.css']
+})
+export class SolicitudComponent implements OnInit {
+
+  solicitud = {nombre: "Javier"};
+
+  constructor() {
+    setInterval(() => {
+      this.solicitud.nombre = '' + Math.random();
+    }, 2000)
+  }
+
+  ngOnInit(): void {
+  }
+
+}
